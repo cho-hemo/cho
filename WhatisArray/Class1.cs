4fe21f126d463e788687fcca9d8b23891caa9ec3
DESKTOP-0G88Q6M\KGA_18 <ggdp9512@gmail.com>
2022-12-21 17:31:02 +0900

    22.12.21

---
+++ WhatisArray/Class1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace WhatisArray
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            // 22.12.21 11:50~
+
+            /**
+             * 숫자 야구
+             * 3자리 숫자를 임의로 정한 뒤 사용자가 3자리 숫자를 맞추게 함
+             * 숫자는 맞고 자리만 맞은 경우 볼.
+             * 숫자와 자리가 모두 맞은 경우 스트라이크.
+             * 아무것도 맞지 않았으면 아웃.
+             */
+
+            Random random= new Random();
+            int Num1 = random.Next(1, 10);
+            int Num2 = random.Next(1, 10);
+            while (Num1 == Num2)
+            {
+                Num2 = random.Next(1, 10);
+            }
+            int Num3 = random.Next(1, 10);
+            while (Num3 == Num2 || Num3 == Num1)
+            {
+                Num3 = random.Next(1, 10);
+            }
+
+            
+            int round = 0;
+
+            int strike = 0;
+            int ball = 0;
+            int userNum1 = 0;
+            int userNum2 = 0;
+            int userNum3 = 0;
+
+            bool isUserCollect1 = userNum1 == Num1;
+            bool isUserCollect2 = userNum2 == Num2;
+            bool isUserCollect3 = userNum3 == Num3;
+
+            Console.WriteLine($"{Num1} {Num2} {Num3} ");
+
+            while (!isUserCollect1 || !isUserCollect2 || !isUserCollect3)
+            {
+
+                round++;
+                if (round >= 9)
+                    break;
+
+                Console.Write("첫번째 숫자를 입력해주세요. -> ");
+                int.TryParse(Console.ReadLine(), out userNum1);
+
+                while (userNum1 > 9 || userNum1 < 0)
+                {
+                    Console.Write("첫번째 숫자를 0~9 중 입력해주세요. -> ");
+                    int.TryParse(Console.ReadLine(), out userNum1);
+                }
+
+                Console.Write("두번째 숫자를 입력해주세요. -> ");
+                int.TryParse(Console.ReadLine(), out userNum2);
+
+                while (userNum1 == userNum2 || userNum2 > 9 || userNum2 < 0)
+                {
+                    Console.Write("서로 다른 두번째 숫자를 0~9 중 입력해주세요. -> ");
+                    int.TryParse(Console.ReadLine(), out userNum2);
+                }
+
+                Console.Write("마지막 숫자를 입력해주세요. -> ");
+                int.TryParse(Console.ReadLine(), out userNum3);
+
+                while (userNum2 == userNum3 || userNum1 == userNum3 || userNum3 > 9 || userNum3 < 0)
+                {
+                    Console.Write("서로 다른 마지막 숫자를 0~9 중 입력해주세요. -> ");
+                    int.TryParse(Console.ReadLine(), out userNum3);
+                }
+
+                isUserCollect1 = userNum1 == Num1;
+                isUserCollect2 = userNum2 == Num2;
+                isUserCollect3 = userNum3 == Num3;
+
+
+                strike = 0;
+                ball = 0;
+
+                bool isUserBall1 = userNum1 == Num2 || userNum1 == Num3;
+                bool isUserBall2 = userNum2 == Num1 || userNum2 == Num3;
+                bool isUserBall3 = userNum3 == Num1 || userNum3 == Num2;
+
+                if (isUserBall1)
+                    ball++;
+                if(isUserBall2)
+                    ball++;
+                if(isUserBall3)
+                    ball++;
+
+                if (isUserCollect1)
+                    strike++;
+                if (isUserCollect2)
+                    strike++;
+                if (isUserCollect3)
+                    strike++;
+
+                if (ball == 0 && strike == 0)
+                    Console.WriteLine("아웃");
+                else if (strike == 3)
+                    break;
+                else
+                    Console.WriteLine($"{strike}S {ball}B");
+
+            }
+            if(round>=9)
+            Console.WriteLine("9회말 아웃. 컴퓨터의 승리입니다.");
+            else
+            Console.WriteLine($"{strike}S {ball}B 정답입니다.");
+
+
+            //Console.Write("3자리 숫자를 맞춰보세요. 각 숫자는 띄어쓰기로 구분합니다. -> ");
+            //string[] userNum = Console.ReadLine().Split(' ');
+
+            //while(userNum.Length > 3 && userNum.Length <= 0) 
+            //{
+            //    Console.Write("3자리 숫자를 맞춰보세요. 각 숫자는 띄어쓰기로 구분합니다. -> ");
+            //    userNum = Console.ReadLine().Split(' ');
+            //}
+
+
+        }
+
+    }
+}
---
+++ WhatisArray/Class2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace WhatisArray
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * 22.12.21 오후 마지막 시간
+             * 
+             * 컬렉션(Collection) 또는 컨테이너(Container)
+             * 이름 하나로 데이터 여러 개를 담을 수 있는 자료 구조
+             * 배열(Array), 리스트(List), 사전(Dictinary) 등이 포함
+             * 
+             * 배열(Array)
+             * 같은 종류의 데이터들이 순차적으로 메모리에 저장되는 구조.
+             * 각각의 데이터들은 인덱스를 사용하여 독립적으로 접근.
+             * 편리하게 데이터를 모아서 관리.
+             * 
+             * 배열의 특징
+             * 1. 배열 하나에는 데이터 형식 한 종류만 보관 가능
+             * 2. 배열은 메모리의 연속된 공간을 미리 할당하고, 이를 대괄호([])와 0부터 시작하는 정수형 인덱스로 접근.
+             * 3. 배열을 선언할 때는 new 키워드로 배열을 생성한 후 사용할 수 있다.
+             * 4. 배열에서 값 하나는 요소(Element) 또는 항목(Item)으로 표현.
+             * 5. 필요한 데이터 개수를 정확히 정한다면 메모리를 적게 사용하여 프로그램 크기가 줄고 성능 향상.
+             * 
+             * 배열 종류
+             * 1차원 배열 : 배열의 첨자를 하나만 사용
+             * 다차원 배열 : 첨자 2개 이상을 사용하는 배열(2차원, 3차원, ...n차원 배열)
+             * 가변(Jagged) 배열 : '배열의 배열'. 이름 하나로 다양한 차원의 배열을 표현할 때 사용.
+             * 
+             */
+
+            // 배열의 선언과 초기화
+            int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
+
+            //int number1 = 1;
+            //int number2 = 2;
+            //int number3 = 3;
+            //int number4 = 4;
+            //int number5 = 5;
+            Console.WriteLine();
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.Write($"{numbers[i]}");
+            //}
+
+            //foreach(int element in numbers)
+            //{
+            //    Console.Write(element);
+            //}
+
+        }
+    }
+}
