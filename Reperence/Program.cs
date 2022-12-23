727ce3b35ec75f8b45c7f3052d1ba399035f15dc
DESKTOP-0G88Q6M\KGA_18 <ggdp9512@gmail.com>
2022-12-23 17:40:55 +0900

    22.12.23

---
+++ Reperence/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Reperence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * 22.12.23
+             * 레퍼런스 게임 직접 해보면서 지금까지 배운 것들로 무엇을 어디까지 구현 가능한지
+             * 마인드맵으로 그려보기 아래의 4장 정도
+             *  - 타이틀 씬
+             *  - 선택지를 포함한 이벤트 -> 유저가 뭔가 선택 가능
+             *  - 보상 OR 페널티를 얻을 수 있는 이벤트 -> 유저가 골드, 아이템, 스탯을 얻거나 잃기
+             *  - 전투 씬 -> 몹이 등장해 싸워서 결과를 내는 형태
+             *  
+             */
+
+            string line = "=============================\n";
+            string title = "        여정의 속삭임\n\n\n\n\n\n\n  아무 키나 눌러 시작하세요.\n\n";
+            
+            Console.WriteLine(line+"\n");
+            Console.WriteLine(title);
+            Console.WriteLine(line);
+            if (Console.ReadLine() != string.Empty)
+            {
+                Console.Clear();
+            }
+
+            string charName, start;
+            int charSel, str, obs, hand, endu, intel, luck, gold, power, level, turn, per, eventPer, item;
+            // 랜덤 직업, 근력, 관찰, 손재주, 지구력, 지력, 운,  골드,  전투력,   레벨,  턴,  퍼센트, 이벤트퍼센트, 아이템번호
+            bool ingGame = false;
+            Random random= new Random();
+
+
+            // 게임을 계속할 때 초기화용
+            while (!ingGame)
+            {
+                ingGame = true;
+                charSel = random.Next(1, 4);
+                charName = "0";
+                str = 0;
+                obs = 0;
+                hand = 0;
+                endu = 0;
+                intel = 0;
+                luck = 0;
+                item = 0;
+                switch (charSel)
+                {
+                    case 1:
+                    default:
+                        charName = "약초꾼";
+                        start = "작은 마을에는 더이상 약초가 없다.\n당신은 여행을 떠나기로 했다.\n\n\n";
+                        str = 2;
+                        obs = 5;
+                        hand = 5;
+                        endu = 3;
+                        intel = 2;
+                        luck = 3;
+                        item= 1;
+                        break;
+                    case 2:
+                        charName = "용병";
+                        start = "작은 마을에는 더이상 의뢰가 없다.\n당신은 여행을 떠나기로 했다.\n\n\n";
+                        str = 5;
+                        obs = 2;
+                        hand = 3;
+                        endu = 5;
+                        intel = 2;
+                        luck = 3;
+                        item= 2;
+                        break;
+                    case 3:
+                        charName = "사제";
+                        start = "작은 마을에는 더이상 인도할 사람이 없다.\n당신은 여행을 떠나기로 했다.\n\n\n";
+                        str = 2;
+                        obs = 3;
+                        hand = 3;
+                        endu = 2;
+                        intel = 5;
+                        luck = 5;
+                        item = 3;
+                        break;
+                }
+
+                gold = 0;
+                power = 0;
+                level = 0;
+                turn = 0;
+                per = 30;
+                eventPer = 0;
+
+                // 레벨 7이 되면 엔딩
+                while (level>=7)
+                {
+                    Console.WriteLine(line);
+                    Console.WriteLine($"직업 {charName}");
+                    Console.WriteLine($"근력{str} 관찰{obs} 재주{hand}");
+                    Console.WriteLine($"근성{endu} 지력{intel} 운{luck}");
+                    Console.WriteLine(line);
+                }
+
+            }
+
+            Console.WriteLine("게임이 종료되었습니다.");
+
+
+
+        }
+    }
+}
---
+++ Study1223/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Study1223
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             *  사용자로부터 2개의 문자열을 읽어 같은지 다른지 화면에 출력하는 프로그램 작성
+             *  - Equals 메서드 사용 불가
+             */
+
+            Console.Write("첫 번째 문자열: -> ");
+            string word1 = Console.ReadLine();
+            Console.Write("두 번째 문자열: -> ");
+            string word2 = Console.ReadLine();
+
+            if (word1.Length == word2.Length)
+            {
+                for (int i = 0; i < word1.Length; i++)
+                {
+                    if (word1.Substring(i,1)!=word2.Substring(i,1))
+                    {
+                        Console.WriteLine("두 문자열은 다릅니다.\n");
+                        break;
+                    }
+                    else if(i == word1.Length-1)
+                    {
+                        Console.WriteLine("두 문자열은 같습니다.\n");
+                        
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("두 문자열은 다릅니다.\n");
+            }
+
+            /**
+             * 5개의 음료(콜라, 물, 스프라이트, 주스, 커피)를 판매하는 자판기 머신을 구현
+             * 사용자가 1~5 사이의 숫자를 입력
+             * 선택한 음료를 출력
+             * 그 외의 숫자를 선택하면 오류 메시지
+             */
+
+
+            Console.WriteLine("1. 콜라 2. 물 3. 스프라이트 4. 주스 5.커피");
+            Console.Write("원하는 음료의 숫자를 입력하세요. ->");
+            int drink = 0;
+            int.TryParse(Console.ReadLine(), out drink);
+            while (drink < 1||drink>5)
+            {
+                Console.Write("1과 5사이의 숫자를 입력하세요. ->");
+                int.TryParse(Console.ReadLine(), out drink);
+            }
+
+            switch (drink)
+            {
+                case 1:
+                    Console.WriteLine("콜라가 나왔습니다.");
+                    break;
+                case 2:
+                    Console.WriteLine("물이 나왔습니다.");
+                    break;
+                case 3:
+                    Console.WriteLine("스프라이트가 나왔습니다.");
+                    break;
+                case 4:
+                    Console.WriteLine("주스가 나왔습니다.");
+                    break;
+                case 5:
+                    Console.WriteLine("커피가 나왔습니다.");
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine();
+
+
+            /**
+             * 배열 days[]를 아래와 같이 초기화 하고 배열 요소의 값을 다음과 같이 출력하는 프로그램 작성
+             * - 배열 days[]는 31,29,31,30,31,30,31,31,30,31,30,31
+             * 배열의 초기화는 중괄호를 사용.
+             */
+
+            int[] days = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine($"{i+1}월은 {days[i]}일까지 입니다.");
+            }
+        }
+    }
+}
